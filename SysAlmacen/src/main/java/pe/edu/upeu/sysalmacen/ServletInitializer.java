822cacc24aa3e6104b65b1d05f17a49b306160ea
alexpehu <mamanipari@gmail.com>
2026-05-03 13:38:41 -0500

    prueba de funcionamiento de pepilene cambio2

--- SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/ServletInitializer.java
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-    //Pruebas de cambios para probar pepilene
+    //Pruebas de cambios para probar pepilene segunda prueba
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SysAlmacenApplication.class);
