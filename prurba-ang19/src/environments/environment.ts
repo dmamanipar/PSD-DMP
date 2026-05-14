1fb3c690eaf2cea81b77f22374a9791d655b1a2d
alexpehu <mamanipari@gmail.com>
2026-05-14 00:59:48 -0500

    Ajusta para comunicacion dentro de tomcat

--- prurba-ang19/src/environments/environment.ts
+++ prurba-ang19/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   //HOST:'http://10.80.96.255:8080/SysAlmacen',
   //HOST:'http://docker.tomcat:8080/SysAlmacen',
-  HOST:'http://tomcat-psd:8080/SysAlmacen',
+  HOST:'http://localhost:8080/SysAlmacen',
   //HOST:'http://localhost:8080',
   RETRY: 2,
   TOKEN_NAME: 'access_token',
